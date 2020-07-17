5468c849b04c669d1ee4c781e8282086b269be3a
Artiganage <55954146+Artiganage@users.noreply.github.com>
2020-07-17 17:26:32 +0530

    Day3_Assignent2

---
+++ Day3_Ass2.js
@@ -0,0 +1,27 @@
+let grade=prompt("Enter your grade ");
+switch (grade) {
+    case 'A+':
+        console.log("Pass with EXCELLENCE");
+        break;
+
+    case 'A':
+        console.log("Pass with FIRST CLASS");
+        break;
+
+    case 'B+':
+        console.log("Pass with SECOND CLASS");
+        break;
+
+    case 'B':
+        console.log("PASSED");
+        break;
+
+    case 'C+':
+        console.log("Passed with AGGREGATE");
+        break;
+
+    default:
+        console.log("FAILED");
+        break;
+}
+
